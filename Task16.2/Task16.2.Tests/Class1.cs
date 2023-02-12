96813c7e6ee1ac873a1f3384d4a3dd8d4785c128
Alex <Alex@HOME-PC>
2023-02-12 21:55:17 +0300

    Task16.2

---
+++ Task16.2/Task16.2.Tests/Class1.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task16._2.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(1, 1) == 2);
+        }
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(1, 1) == 0);
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(2, 1) == 2);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+        }
+    }
+}
